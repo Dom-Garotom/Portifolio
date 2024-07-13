97bd81d2af58addf6633bdb0ba2e63939efb92a0
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-07-13 15:34:28 -0300

    feat : creating component the pages hero.

---
+++ src/components/HeroPages/index.tsx
@@ -0,0 +1,33 @@
+import { ReactNode } from "react";
+import LinkDefault from "../sectionProject/linkDefault";
+
+interface HeroPagesProps{
+    title : string,
+    subTitle : string,
+    content : string,
+    href : string,
+    linkText : string,
+    children ?: ReactNode;
+}
+
+function HerosPages(prop : HeroPagesProps) {
+    return (
+        <section className="flex flex-col items-center justify-center gap-5  bg-hero-image bg-cover bg-no-repeat h-[100vh]">
+
+            <div className="flex flex-col items-center text-center gap-3">
+                <span className="text-emerald-500 font-light text-lg">. . /{prop.subTitle}</span>
+                <h1 className="text-5xl font-mono font-semibold mb-4">{prop.title}</h1>
+                <div className="">
+                    <p className="w-[640px] text-gray-500 ">{prop.content}</p>
+                </div>
+            </div>
+
+            {prop.children}
+
+            <LinkDefault href={prop.href} linkText={prop.linkText} left />
+
+        </section>
+    )
+}
+
+export default HerosPages;
