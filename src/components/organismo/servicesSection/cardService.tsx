d762a9e39544d5cfb2bc8bdc50eae669676d46a2
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-10-13 09:15:36 -0300

    refactor: atualizando o responsivo da section services

--- src/components/organismo/servicesSection/cardService.tsx
+++ src/components/organismo/servicesSection/cardService.tsx
@@ -5,9 +5,9 @@ interface CardServiceProp {
 
 function CardService({ title, content }: CardServiceProp) {
     return (
-        <div className="flex flex-col w-full max-w-xl gap-3  border rounded-lg py-10 px-5 bg-transparent shadow-lg transition-transform transform hover:scale-105 hover:shadow-2xl border-gray-500">
+        <div className="flex flex-col w-full max-w-xl gap-3 cursor-pointer  border rounded-lg py-10 px-5 bg-transparent shadow-lg transition-transform transform hover:scale-105 hover:shadow-2xl hover:bg-slate-950 border-gray-500">
             <h2 className="text-xl sm:text-2xl md:text-3xl font-semibold text-slate-100-800">{title}</h2>
-            <p className="text-xs sm:text-base md:text-lg font-medium text-gray-500">{content}</p>
+            <p className="text-xs sm:text-base md:text-lg font-medium text-gray-500 hover:text-emerald-600">{content}</p>
         </div>
     );
 }
--- src/components/organismo/servicesSection/index.tsx
+++ src/components/organismo/servicesSection/index.tsx
@@ -1,32 +1,32 @@
-import ButtonCTA from "../../moleculas/ButtonCTA";
-import CardSkill from "../competence/cardSkill";
 import TitleDefault from "../competence/title";
-import ItenSkill from "../hero/itenSkill";
-import Link from "next/link";
-import { FaArrowRightLong } from "react-icons/fa6";
 import CardService from "./cardService";
-
+import { MdOutlineQuestionMark } from "react-icons/md";
 
 
 function ServiceSection() {
     return (
-        <section className=" flex flex-col items-center justify-around pt-10 bg-gray-900 w-[99vw] pb-40 ">
-            <div className="flex flex-col gap-20 justify-between">
-                <TitleDefault title="Como posso te ajudar ?" subTitle="Serviços" />
+        <section className=" flex flex-col items-center justify-around pt-10 bg-gray-900 w-[99vw] pb-32 ">
+            <div className="flex flex-col gap-10 justify-between  w-[80vw]  ">
+                <TitleDefault title="Como posso te ajudar..." subTitle="Serviços"  />
 
-                <div className="flex flex-col px-10 md:flex-row gap-4 w-[80vw] ">
+                <div className="flex lg:flex-row  flex-col-reverse  gap-4 max-w-[80vw] w-ful items-center justify-between">
 
-                    <CardService
-                        title="Designer UX/UI"
-                        content="Crio layouts e designs atraentes para interfaces de usuário, garantindo uma experiência visualmente agradável e intuitiva com o uso de ferramentas como o figma."
-                    />
+                    <div className="flex flex-col justify-center items-center gap-4 ">
+                        <CardService
+                            title="Designer UX/UI"
+                            content="Crio layouts e designs atraentes para interfaces de usuário, garantindo uma experiência visualmente agradável e intuitiva com o uso de ferramentas como o figma."
+                        />
 
-                    <CardService
-                        title="Desenvolvedor web"
-                        content="Com conhecimentos aplicados em projetos de sites e aplicações web, te ajudo na criação de sites interativos e responsivos usando  frameworks como React e Next js e a magia da estilização advinda do  Tailwind e Sass ."
-                    />
+                        <CardService
+                            title="Desenvolvedor web"
+                            content="Com conhecimentos aplicados em projetos de sites e aplicações web, te ajudo na criação de sites interativos e responsivos usando  frameworks como React e Next js e a magia da estilização advinda do  Tailwind e Sass ."
+                        />
+                    </div>
 
-                    
+
+                    <div className="flex w-full items-center justify-center max-w-[400px]">
+                        <MdOutlineQuestionMark className="text-[20rem] text-slate-800 hover:text-emerald-600" />
+                    </div>
 
                 </div>
 
