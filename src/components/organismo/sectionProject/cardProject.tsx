b41bd3b8020f162cc0bcd92ae7811ed70b8d2ae7
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-10-16 11:34:51 -0300

    feat: adicionando animação no componente dos cards do projeto

--- src/components/organismo/sectionProject/cardProject.tsx
+++ src/components/organismo/sectionProject/cardProject.tsx
@@ -1,11 +1,10 @@
 import Image, { StaticImageData } from "next/image";
-import ButtonCTA from "../../moleculas/ButtonCTA";
-import ItenSkill from "../hero/itenSkill";
 import foto from "../../../../public/images/Foto (1).png";
 import { CgPathExclude } from "react-icons/cg";
 import { FaArrowRightLong } from "react-icons/fa6";
 import React, { ReactNode } from "react";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 interface CardProjectProp {
     title: string;
@@ -17,7 +16,14 @@ interface CardProjectProp {
 
 function CardProject(prop: CardProjectProp) {
     return (
-        <div className="flex flex-col lg:flex-row w-full border-y py-10 gap-9 border-gray-700">
+        <motion.div
+            className="flex flex-col lg:flex-row w-full border-y py-10 gap-9 border-gray-700"
+            whileInView={{ x: 0 , opacity:1 }}
+            initial={{x:-100 , opacity: 0}}
+            transition={{ type:"tween", duration: 0.6}}
+
+
+        >
             <div className="w-full w-auto lg:min-w-[450px] lg:w-[500px]">
                 <Image src={prop.image === undefined ? foto : prop.image} width={450} height={450} alt={prop.title} className="w-full h-full " />
             </div>
@@ -32,14 +38,15 @@ function CardProject(prop: CardProjectProp) {
                     {prop.children}
                 </div>
                 <div className="flex gap-2 flex-wrap w-full md:w-[300px]">
-                    
+
                 </div>
-                <Link href={prop.url} target="_blank" className="flex items-center gap-2 text-gray-300 mt-4 w-32 hover:font-bold transition-all">
+                <Link href={prop.url} className="flex items-center gap-2 text-gray-300 mt-4 w-32 hover:font-bold transition-all">
                     Ver projeto
                     <FaArrowRightLong />
                 </Link>
             </div>
-        </div>
+
+        </motion.div>
     );
 }
 
