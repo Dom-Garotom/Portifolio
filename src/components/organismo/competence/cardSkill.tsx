cbf58c46346c4b4171dfba789e4be9e0f4ce8a3c
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-10-16 11:21:52 -0300

    feat: adicionando animação no componente de card skill e implementado ele na página

--- src/components/organismo/competence/cardSkill.tsx
+++ src/components/organismo/competence/cardSkill.tsx
@@ -1,21 +1,27 @@
-import { ReactElement } from "react";
+"use client"
 
-interface CardSkillProps {
+import {   ComponentProps, ReactElement } from "react";
+import { motion } from "framer-motion";
+
+type CardSkills = ComponentProps<typeof motion.div> & {
     title: string,
     subTitle: string,
     icon?: ReactElement,
 }
 
-function CardSkill(prop: CardSkillProps) {
+function CardSkill({title , subTitle , icon , ...motionsProps}: CardSkills) {
     return (
-        <div className="flex flex-col justify-between p-5 bg-gray-800 text-gray-500 text-lg gap-3 min-w-[200px]
-                        max-w-[600px] w-full h-[7rem] rounded-lg hover:bg-emerald-500 hover:text-white transition-all ">
+        <motion.div
+        {...motionsProps}
+        className="flex flex-col justify-between p-5 bg-gray-800 text-gray-500 text-lg gap-3 min-w-[200px]
+        max-w-[600px] w-full h-[7rem] rounded-lg hover:bg-emerald-500 hover:text-white transition-all "
+        >
             <div className="flex justify-between w-full ">
-                <p className="font-medium">{prop.title}</p>
-                <span className="font-normal">{prop.icon}</span>
+                <p className="font-medium">{title}</p>
+                <span className="font-normal">{icon}</span>
             </div>
-            <p className="w-full text-base">{prop.subTitle}</p>
-        </div>
+            <p className="w-full text-base">{subTitle}</p>
+        </motion.div>
     );
 }
 
--- src/components/organismo/competence/index.tsx
+++ src/components/organismo/competence/index.tsx
@@ -9,18 +9,58 @@ import TitleDefault from "./title";
 
 
 function CompetenciaSection() {
+    const skillsData = [
+        {
+            title: "Typescript",
+            subTitle: "Nível intermediário",
+            icon: <SiTypescript />
+        },
+        {
+            title: "Node",
+            subTitle: "Nível intermediário",
+            icon: <RiJavascriptLine />
+        },
+        {
+            title: "Next",
+            subTitle: "Nível intermediário",
+            icon: <RiNextjsFill />
+        },
+        {
+            title: "React",
+            subTitle: "Nível intermediário",
+            icon: <FaReact />
+        },
+        {
+            title: "Tailwind",
+            subTitle: "Nível intermediário",
+            icon: <RiTailwindCssFill />
+        },
+        {
+            title: "Styled Components",
+            subTitle: "Nível intermediário",
+            icon: <RxComponent1 />
+        }
+    ];
+
     return (
         <section className=" flex flex-col items-center justify-around  bg-gray-900 w-[99vw] py-20">
             <div className="flex flex-col gap-20 justify-between px-2">
                 <TitleDefault title="Conhecimentos" subTitle="competências" />
 
                 <div className="grid grid-cols-1 px-2 mobile:px-0  mobile:grid-cols-2  md:grid-cols-3 gap-4 w-[95vw] lg:w-[80vw] ">
-                    <CardSkill title="Typescript" subTitle="Nível intermédiario" icon={<SiTypescript />} />
-                    <CardSkill title="Node" subTitle="Nível intermédiario" icon={<RiJavascriptLine />} />
-                    <CardSkill title="Next" subTitle="Nível intermédiario" icon={<RiNextjsFill />} />
-                    <CardSkill title="React" subTitle="Nível intermédiario" icon={<FaReact />} />
-                    <CardSkill title="Tailwnd" subTitle="Nível intermédiario" icon={<RiTailwindCssFill />} />
-                    <CardSkill title="Styled Component" subTitle="Nível intermédiario" icon={<RxComponent1/>} />
+                    {skillsData.map((skill, index) => (
+                        <CardSkill
+                            key={index}
+                            title={skill.title}
+                            subTitle={skill.subTitle}
+                            icon={skill.icon}
+
+                            initial={{y:-50,  opacity:0}}
+                            whileInView={{y:0,  opacity:1}}
+                            exit={{y:-50,  opacity:0}}
+                            transition={{duration: 0.1 , delay: index * 0.1}}
+                        />
+                    ))}
                 </div>
             </div>
         </section>
